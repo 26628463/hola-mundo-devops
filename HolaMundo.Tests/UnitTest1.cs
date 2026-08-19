02ed2f138010a138480bf33a407371907a277db4
Skarnx <abreusketty26@gmail.com>
2026-08-18 22:40:01 -0400

    Proyecto inicial Hola Mundo DevOps

---
+++ HolaMundo.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace HolaMundo.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void GetGreeting_ReturnsHolaMundo()
+    {
+        var resultado = "Hola Mundo desde DevOps!";
+
+        Assert.Equal("Hola Mundo desde DevOps!", resultado);
+    }
+}
